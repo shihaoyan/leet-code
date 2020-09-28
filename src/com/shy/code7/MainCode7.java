fca6ca8e5fe2125a1164133ceb811d0a1cd3ddd2
shihaoyan <923463567@qq.com>
2020-09-28 20:40:37 +0800

    最长公共前缀

--- src/com/shy/code7/MainCode7.java
+++ src/com/shy/code7/MainCode7.java
@@ -18,8 +18,21 @@ public class MainCode7 {
         if (strs.length == 0) {
             return "";
         }
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            for (; j < ans.length() && j < strs[i].length(); j++) {
+                if (ans.charAt(j) != strs[i].charAt(j)) {
+                    break;
+                }
+                ans = ans.substring(0, j);
+                if ("".equals(ans)) {
+                    return ans;
+                }
+            }
+        }
+        return ans;
 
-        return null;
     }
 
 }
