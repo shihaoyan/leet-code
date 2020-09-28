82afb2456b5f7bdd95207d99d32df6e0427d5fe0
haoyan.shi <haoyan.shi@nio.com>
2020-09-28 13:45:16 +0800

    最长公共前缀（未完成）

--- src/com/shy/code7/MainCode7.java
+++ src/com/shy/code7/MainCode7.java
@@ -4,8 +4,22 @@ package com.shy.code7;
  * @author haoyan.shi
  */
 public class MainCode7 {
+    /**
+     * 最长公共前缀
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+
+
+
+
+
+        return null;
     }
 
 }
