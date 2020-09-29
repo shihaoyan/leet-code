670df161f82667408f5341d45dd05bf30ba54a2d
shihaoyan <923463567@qq.com>
2020-09-29 08:34:39 +0800

    合并两个有序链表

---
+++ src/com/shy/code9/MainCode9.java
@@ -0,0 +1,18 @@
+package com.shy.code9;
+
+import com.shy.common.ListNode;
+
+/**
+ * @author 石皓岩
+ * @date 2020/9/29 8:31
+ * 描述:合并两个有序链表
+ */
+public class MainCode9 {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
---
+++ src/com/shy/common/ListNode.java
@@ -0,0 +1,23 @@
+package com.shy.common;
+
+/**
+ * @author 石皓岩
+ * @date 2020/9/29 8:32
+ * 描述:
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
