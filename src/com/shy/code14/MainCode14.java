b442228d8199752a3b6d43ff6d84835cfe846b07
shihaoyan <923463567@qq.com>
2020-10-01 20:58:04 +0800

    外观数列（未完成）

---
+++ src/com/shy/code14/MainCode14.java
@@ -0,0 +1,16 @@
+package com.shy.code14;
+
+/**
+ * @author 石皓岩
+ * @date 2020/10/1 20:53
+ * 描述:外观数列
+ */
+public class MainCode14 {
+    public static void main(String[] args) {
+
+    }
+
+    public static String countAndSay(int n) {
+        return null;
+    }
+}
