87050038d988d565accc3a3f5a4bd9a6eb633f00
shihaoyan <923463567@qq.com>
2020-10-01 13:24:29 +0800

    删除排序数组中的重复元素

--- src/com/shy/code10/MainCode10.java
+++ src/com/shy/code10/MainCode10.java
@@ -11,11 +11,16 @@ public class MainCode10 {
     }
 
     public static int removeDuplicates(int[] nums) {
-
-
-
-
-
-        return 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
     }
 }
