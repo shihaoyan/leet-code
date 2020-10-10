9a7b589ebf06419c60ec62410a873686307102e9
haoyan.shi <haoyan.shi@nio.com>
2020-10-10 16:46:25 +0800

    外观数列

--- src/com/shy/code14/MainCode14.java
+++ src/com/shy/code14/MainCode14.java
@@ -7,17 +7,42 @@ package com.shy.code14;
  */
 public class MainCode14 {
     public static void main(String[] args) {
-
+        System.out.println(countAndSay(5));
     }
 
+    /**
+     * 1.     1
+     * 2.     11
+     * 3.     21
+     * 4.     1211
+     * 5.     111221
+     * @param n
+     * @return
+     */
     public static String countAndSay(int n) {
 
-        // 个位数
-        int k = n % 10;
-        // 十位数
-        int j = n / 10;
-
-        return null;
+        // 递归终止条件
+        if (n == 1) {
+            return "1";
+        }
+        StringBuffer res = new StringBuffer();
+        // 拿到上一层的字符串
+        String str = countAndSay(n - 1);
+        int length = str.length();
+        // 开始指针为0
+        int start = 0;
+        // 注意这从起始条件要和下面长度统一
+        for (int i = 1; i < length + 1; i++) {
+            // 字符串最后一位直接拼接
+            if (i == length) {
+                res.append(i - start).append(str.charAt(start));
+                // 直到start位的字符串和i位的字符串不同，拼接并更新start位
+            } else if (str.charAt(i) != str.charAt(start) ) {
+                res.append(i - start).append(str.charAt(start));
+                start = i;
+            }
+        }
+        return res.toString();
 
     }
 }
