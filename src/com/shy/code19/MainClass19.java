065f86a4afc15b2bbe89c1ccfd1022a384ddb3d6
shihaoyan <shihaoyan@jd.com>
2020-12-24 18:00:01 +0800

    分发糖果未完成

---
+++ src/com/shy/code19/MainClass19.java
@@ -0,0 +1,17 @@
+package com.shy.code19;
+
+/**
+ * @author haoyan.shi
+ * @createDate 2020/12/24 17:43
+ * description:分发糖果
+ */
+public class MainClass19 {
+    public static void main(String[] args) {
+
+    }
+
+    public int candy(int[] ratings) {
+
+        return 0;
+    }
+}
